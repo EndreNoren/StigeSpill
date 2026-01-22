1cf3c6f762091f67faa1d77ba768e556d92b5004
184587 <endrenoren@gmail.com>
2026-01-22 13:08:54 +0100

    First

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random t = new Random();
+
+        int kast = t.nextInt(6);
+        kast++;
+
+        System.out.println("Du kastet " + kast);
+
+
+
+
+
+    }
+}
