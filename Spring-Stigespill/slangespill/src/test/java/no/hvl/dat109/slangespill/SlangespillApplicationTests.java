18c332597485db41637e2dc4d4b26ab9462f9130
186510kristoffer <krissalbrigtsen@gmail.com>
2026-01-22 13:33:47 +0100

    Spring prosjektet

---
+++ Spring-Stigespill/slangespill/src/test/java/no/hvl/dat109/slangespill/SlangespillApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.slangespill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlangespillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
